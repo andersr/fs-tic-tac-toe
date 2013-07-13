68b60a8fde2d3161065af9c356c5f6f11df7f18a
Anders Ramsay <andersr@gmail.com>
2013-07-13 12:58:00 -0400

    basic tic tac toe but not checking for win or tie

--- js/main.js
+++ js/main.js
@@ -1,4 +1,58 @@
+var current_turn;
+
 $( "#start-game" ).click(function () {
-    $("#intro").hide();
+    startGame();
 });
 
+function startGame() {
+	$("#game-setup").hide();
+	$("#play-game").show();
+
+	// X always has the first turn
+	current_turn = 'X';
+	playGame();
+};
+
+// function gameSquare(location, state) {
+//   this.location = location;
+//   this.state = state;
+// }
+
+function playGame() {
+
+	//create an array of game_squares
+	//game_board = new Array(8);
+
+	//loop through these for each game_board
+	//var game_board[i] = new gameSquare(1-1, "empty");
+
+	//add click event listeners
+	$( "#game-board button" ).click(function (event) {
+
+		//find out which square was clicked on
+		var clicked_on_square = event.target.id;
+
+		// replace the button with X or O based on current_turn
+		$(this).replaceWith('<span class="xo">' + current_turn + '</span>');
+
+		switchPlayer();
+
+	});
+
+}
+
+function switchPlayer(){
+
+if(current_turn == 'X'){
+			current_turn = 'O';
+		} else {
+			current_turn = 'X';
+		}
+	return current_turn;
+
+		//switchPlayer(current_turn);
+
+	//playGame(current_turn);
+}
+
+//check for a win or tie, else continue game

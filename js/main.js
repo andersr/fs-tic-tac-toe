88cb87dc8ad2d928fb616c34f17c87fe82d5e8c6
Anders Ramsay <andersr@gmail.com>
2013-07-14 16:33:26 -0400

    everything working except ending game after there is a winner

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
 //global variables
-var board_size = 3; //hardcoded 3x3 board for now
-var game_board = new Array((board_size * board_size) - 1);
-var remaining_moves = game_board.length + 1; //9 moves for a 3x3 board
+var board_size = 3;
+var game_board_squares = new Array(board_size * board_size); //this creates
+var remaining_moves = game_board_squares.length - 1;
 var x_mark = 'X';
 var o_mark = 'O';
 var player_x = x_mark;
 var player_o = o_mark;
 var current_player;
 var current_mark;
-
-//winning patterns for a 3x3 board
-var winners_3x3 = [['1','2','3'], ['4','5','6'],['7','8','9']];
+var winning_player;
 
 //game square constructor
 function gameSquare(state) {
@@ -18,8 +16,7 @@ function gameSquare(state) {
 }
 
 function createEmptyGameBoard(board) {
-    var i, n = board.length;
-    for (i = 0; i < n; ++i) {
+    for (var i = 0, l = board.length; i < l; i++) {
         board[i] = new gameSquare("empty");
     }
 
@@ -29,81 +26,121 @@ function createEmptyGameBoard(board) {
 //on click of start-game
 $( "#start-game" ).click(function () {
 
-	createEmptyGameBoard(game_board);
+	winning_player == null;
 
-	//alert(game_board[2].state);
-
-	//create a game board with empty game squares
-	// for(var i = 0; i < game_board.length; i++){
-
-	// 	game_board[i] = new gameSquare("empty");
-	// }
-
-    startGame();
+	createEmptyGameBoard(game_board_squares);
+	
+    startEndGame();
 });
 
-function startGame() {
+function startEndGame() {
 
-	getPlayerNames();
+	if (winning_player == null){
 
-	// update the game header text
+		getPlayerNames();
 
-	$("#game-setup").hide();
-	$("#play-game").show();
+		$("#game-setup").hide();
+		$("#play-game").show();
 
-	current_player = player_x; 	// X always has the first turn in tic tac toe
-	current_mark = x_mark;
+		current_player = player_x; 	// X always has the first turn in tic tac toe
+		current_mark = x_mark;
 
-	$("#alerts").show()
-	$("#alerts span").replaceWith('<span>Player ' + current_player + ', your turn...</span>');
-	
-	playGame();
+		$("#alerts").show()
+		$("#alerts span").replaceWith("<span>" + current_player + ", your turn...</span>");
+		
+		playGame();
 
-}
-
-function getPlayerNames(){
-
-	if($('#player_x_name').val() !== ''){
-		player_x = $("#player_x_name").val() + " (X)";
 	}
+	else {
+		$("#alerts span").replaceWith("<span>" + winning_player + " is the winner!</span>");
 
-	if($('#player_o_name').val() !== ''){
-		player_o = $("#player_o_name").val() + " (O)";
 	}
-
-	return player_x, player_o;
-
 }
 
 function playGame() {
 
-	
 	$( "#game-board button" ).click(function (event) {
 
-		//get the id of the square that was clicked on
 		var clicked_on_square = event.target.id;
+		game_board_squares[clicked_on_square].state = current_mark;
+		$(this).replaceWith("<span class='xo'>" + current_mark + "</span>");
 
-		//set the state of the corresponding square to be the current mark (X or O)
-		game_board[clicked_on_square].state = current_mark;
+		checkForWinner();
 
-		$(this).replaceWith('<span class="xo">' + current_mark + '</span>');
+		if (winning_player == player_o || winning_player == player_x){
+			startEndGame();
+		}
+		else if(remaining_moves == 0) {
 
-		switchPlayer();
-	
-	//switchPlayer();
-		//checkForWinner();
+			$("#alerts span").replaceWith("<span>It's a tie!</span>");
 
-		// remaining_moves -= 1;
-
-		// if(remaining_moves <= (game_board.length - (board_size - 1))) {
-			
-		// } else {
-		// 	switchPlayer();
-		// };
-
-	//	checkForWinner();
+		}
+		else {
+			remaining_moves -= 1;
+			switchPlayer();
+		}
 
 	});
+
+	
+}
+
+function checkForWinner() {
+
+	//winning patterns for a 3x3 board
+	var winners_3x3 = 
+	[['1','2','3'], ['4','5','6'],
+	['7','8','9'],['1','4','7'],
+	['2','5','8'],['3','6','9'],
+	['1','5','9'],['3','5','7']];
+
+
+	//loop through all 3x3 winning patterns
+	for(var i = 0, l = winners_3x3.length; i < l; i++){
+
+		// assign a winning pattern(array) to winner
+		var winner = winners_3x3[i];
+
+		//check to see if all three have either a 'O' or an 'X'
+		// since I am using a zero-based id for matching the pattern, will need to move down by 1
+
+		//if the state of the id corresponding to game_board_squares_squares 0, 1, 2 all are 'O'
+		if(game_board_squares[winner[0] - 1].state == 'O'
+			 && game_board_squares[winner[1] - 1].state == 'O'
+			 && game_board_squares[winner[2] - 1].state == 'O') {
+
+			//alert("O has won!");
+			winning_player ==  player_o;
+			
+
+		} 
+		else if(game_board_squares[winner[0] - 1].state == 'X' 
+			&& game_board_squares[winner[1] - 1].state == 'X'
+			&& game_board_squares[winner[2] - 1].state == 'X') {
+
+			//alert("X has won!");
+			winning_player == player_x;
+		}
+		return winning_player;
+	}
+}
+function getPlayerNames(){
+
+	if($("#player_x_name").val() !== ""){
+		player_x = $("#player_x_name").val() + " (X)";
+	} 
+	else {
+		player_x = "Player X";
+	}
+
+	if($("#player_o_name").val() !== ""){
+		player_o = $("#player_o_name").val() + " (O)";
+	}
+	else {
+		player_o = "Player O";
+	}
+
+	return player_x, player_o;
 }
 
 function switchPlayer(){
@@ -119,40 +156,9 @@ function switchPlayer(){
 		current_mark = x_mark;
 	}
 
-	$("#alerts span").replaceWith('<span>Player ' + current_player + ', your turn...</span>');
+	$("#alerts span").replaceWith("<span>" + current_player + ", your turn...</span>");
 	
 	return current_player, current_mark;
-	//playGame();
-	//return current_player, current_mark;
 }
 
-function checkForWinner() {
 
-	for(var i = 0; i < winners_3x3.length; i++){
-
-		var winner = winners_3x3[i];
-
-		//alert(winner[0]);
-
-		if(game_board[winner[0] - 1 ].state == 'O'
-			 && game_board[winner[1] - 1].state == 'O'
-			 && game_board[winner[2] - 1].state == 'O') {
-
-			alert("O has won!");
-
-		} 
-		else if(game_board[winner[0]].state == 'X' 
-			&& game_board[winner[1]].state == 'X'
-			&& game_board[winner[2]].state == 'X') {
-
-			alert("X has won!");
-
-		}
-		else {
-
-			//alert("no winner yet");
-
-			switchPlayer();
-		}
-	}
-}
